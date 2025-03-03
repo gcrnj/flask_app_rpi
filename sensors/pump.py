c47d02c52ddf039f0e4251675a2003925a37312d
Gio Cornejo - Local <giojenroc@gmail.com>
2025-03-03 15:48:40 +0800

    Added pump functionality

--- sensors/pump.py
+++ sensors/pump.py
@@ -1,9 +1,15 @@
+import cpio
+import RPi.GPIO as GPIO
+
+PUMP_PIN = cpio.Cpio.Pump
 
 def get_pump_status() -> bool:
     return True
 
 def turn_on():
+    GPIO.output(PUMP_PIN, GPIO.LOW)  # Activate relay (pump ON)
     print("Pump turned on")
 
 def turn_off():
+    GPIO.output(PUMP_PIN, GPIO.HIGH)  # Activate relay (pump ON)
     print("Pump turn off")
