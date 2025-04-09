55c7713024e8f58cd147cf2e4c0246bb72dcb14f
GioRpi2 <gio.rpi2@techtitans.com>
2025-04-10 05:12:58 +0800

    imwrite

--- sensors/camera.py
+++ sensors/camera.py
@@ -1,15 +1,18 @@
 import cv2
 from datetime import datetime, timezone, timedelta
-
+import sys
 # Open the webcam (0 is the default camera)
 PH_TZ = timezone(timedelta(hours=8))
 
 def capture_image(device_id):
-    cap = cv2.VideoCapture(0)
+    if sys.platform == 'win32':
+        cap = cv2.VideoCapture(0)
+    else:
+        cap = cv2.VideoCapture(0)
     # Check if the webcam is opened successfully
     if not cap.isOpened():
         print("Error: Could not open camera.")
-        release()
+        release(cap)
         return None
     else:
         # Capture a single frame
@@ -19,8 +22,8 @@ def capture_image(device_id):
             # Save the image
             timestamp = datetime.now(PH_TZ).strftime('%Y%m%d-%H%M%S')
             file_name = f"api/static/{device_id}-{timestamp}.jpg"
-            cv2.imwrite(file_name, frame)
-            print(file_name)
+            isSuccess = cv2.imwrite(file_name, frame)
+            print(f'{isSuccess} - {file_name}')
             return file_name
         else:
             print("Error: Could not capture image.")
@@ -33,3 +36,6 @@ def release(cap):
 
     # Close all OpenCV windows (not needed for this script but useful in GUI apps)
     cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    print(capture_image('Zg6XgWqdztP3bDwquu51'))
